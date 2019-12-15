b18db063922e2153b741873aa44ed717b99742a1
rahulkaushik84 <54129003+rahulkaushik84@users.noreply.github.com>
2019-12-14 20:22:11 -0600

    updated PORT to uppercase

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const hbs = require('hbs')
 const geocode = require('./utils/geocodeutil.js')
 const weather = require('./utils/forecast.js')
 // added to support heroku dynamic port assingment
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 
 const app = express()
